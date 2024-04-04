443ba362b1e23099348b2618214bfde037674b6a
tiemonl <tiemon.liam@gmail.com>
2024-04-04 18:18:37 -0400

    add splash screen

--- app/src/main/java/com/kroger/classapp/MainActivity.kt
+++ app/src/main/java/com/kroger/classapp/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.kroger.classapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.commit
 import com.kroger.classapp.databinding.ActivityMainBinding
 import com.kroger.classapp.ui.CharacterListFragment
+import com.kroger.classapp.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            this.apply {
+                this.
+            }
+            setKeepOnScreenCondition {
+                viewModel.splashScreen.value
+            }
+        }
         setContentView(binding.root)
         supportFragmentManager.commit {
             setReorderingAllowed(true)
---
+++ app/src/main/java/com/kroger/classapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.kroger.classapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _splashScreen = MutableStateFlow(true)
+    val splashScreen = _splashScreen.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(5000L)
+            _splashScreen.value = false
+        }
+    }
+}
